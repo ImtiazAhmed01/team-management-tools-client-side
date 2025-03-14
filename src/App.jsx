0e03ce63a30f7ef6835a15e488663682d4045868
Sais-opu <saidulislamapu2024@gmail.com>
2025-03-14 11:03:14 +0600

    path set login register

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
 
   return (
     <>
-     <Navbar></Navbar>
-     <Outlet></Outlet>
-     <Footer></Footer>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
